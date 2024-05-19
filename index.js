3063fb48995b37cd97e19cc479a3d21de2291429
ZamboniA <andersonzamboni18@gmail.com>
2024-05-19 18:13:27 -0300

    Railway

--- index.js
+++ index.js
@@ -2,31 +2,43 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
 const path = require('path');
+
 const app = express();
+const PORT = process.env.PORT || 3001
+
 app.use(express.json());
+app.use(cors());
 
-mongoose.connect(process.env.MONGODB_URL);
 
-app.use('/uploads', express.static(path.join(__dirname, '/routes/uploads')));
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Erro de conexão com o MongoDB:"));
+db.once("open", () => {
+  console.log("Conectado ao MongoDB!");
+});
+
+
+app.use('/uploads', express.static(path.join(__dirname, 'routes', 'uploads')));
+
 
 const brownieRoutes = require("./routes/brownie/brownie");
 const mousseRoutes = require("./routes/mousse/mousse");
 const paoDeMelRoutes = require("./routes/PaoDeMel/paoDeMel");
-const todosItens = require("./routes/todosItens/todosItens");
-const custos = require("./routes/custos/custos");
+const todosItensRoutes = require("./routes/todosItens/todosItens");
+const custosRoutes = require("./routes/custos/custos");
+
+app.use('/brownie', brownieRoutes);
+app.use('/mousse', mousseRoutes);
+app.use('/paodemel', paoDeMelRoutes);
+app.use('/todositens', todosItensRoutes);
+app.use('/custos', custosRoutes);
 
 
-app.use(cors());
-
-
-app.use(brownieRoutes); 
-app.use(mousseRoutes);
-app.use(paoDeMelRoutes);
-app.use(todosItens);
-app.use(custos);
-
-app.listen(3001, () =>{
-    console.log("Servidor rodando em http://localhost:3001/")
-})
+app.listen(PORT, () => {
+ console.log(`Servidor rodando em http://localhost:${PORT}/`);
+});
