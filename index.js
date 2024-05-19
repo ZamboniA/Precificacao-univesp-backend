42d5e321b158a1e521c7c4409fa5514d224b75a5
ZamboniA <andersonzamboni18@gmail.com>
2024-05-19 18:19:31 -0300

    Deploy railway

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors());
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: false,
+    useCreateIndex: true
 });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Erro de conexão com o MongoDB:"));
